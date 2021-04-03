bf1b875b849daf86b4aa9bec36293d19f61ef02a
aslam karachiwala <4slam@mythicflow.com>
2021-04-03 19:59:33 -0400

    use helmet.hsts() and configure it

--- myApp.js
+++ myApp.js
@@ -8,6 +8,9 @@ app.use(helmet({frameguard: {action: "deny"}}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts());
+timeInSeconds = 90 * 24 * 60 * 60;
+app.use(helmet(hsts: {maxAge: timeInSeconds, force: true}));
 
 
 
