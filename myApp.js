53268a0f299b8af30166ef6221e0bf23cb5dc9ff
aslam karachiwala <4slam@mythicflow.com>
2021-04-03 20:14:54 -0400

    added missing "{}" around "hsts: ..."

--- myApp.js
+++ myApp.js
@@ -10,7 +10,7 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts());
 var timeInSeconds = 90 * 24 * 60 * 60;
-app.use(helmet(hsts: {maxAge: timeInSeconds, force: true}));
+app.use(helmet({hsts: {maxAge: timeInSeconds, force: true}}));
 
 
 
