9bcd2888480b6245ce0d48f0a28de9fe1cef7a8c
aslam karachiwala <4slam@mythicflow.com>
2021-04-03 20:20:04 -0400

    use helmet.noCache()

--- myApp.js
+++ myApp.js
@@ -12,7 +12,7 @@ app.use(helmet.hsts());
 var timeInSeconds = 90 * 24 * 60 * 60;
 app.use(helmet({hsts: {maxAge: timeInSeconds, force: true}}));
 app.use(helmet.dnsPrefetchControl());
-
+app.use(helmet.noCache());
 
 
 
