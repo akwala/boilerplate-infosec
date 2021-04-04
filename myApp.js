049398f304fde0f937c9f71403b029e45236386d
aslam karachiwala <4slam@mythicflow.com>
2021-04-03 21:32:40 -0400

    use helmet() parent

--- myApp.js
+++ myApp.js
@@ -2,21 +2,38 @@ var express = require('express');
 var helmet = require('helmet');
 var app = express();
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard())
+app.use(helmet());
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard())
 app.use(helmet({frameguard: {action: "deny"}}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts());
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// app.use(helmet.hsts());
 var timeInSeconds = 90 * 24 * 60 * 60;
-app.use(helmet({hsts: {maxAge: timeInSeconds, force: true}}));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", 'trusted-cdn.com'],
+// app.use(helmet({hsts: {maxAge: timeInSeconds, force: true}}));
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(helmet.contentSecurityPolicy({
+//   directives: {
+//     defaultSrc: ["'self'"],
+//     scriptSrc: ["'self'", 'trusted-cdn.com'],
+//   },
+// }));
+
+app.use(helmet({
+  frameguard: {
+    action: "deny"
+  },
+  hsts: {
+    maxAge: timeInSeconds, force: true
+  },
+  noCache: true,
+  contentSecurityPolicy:{
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com'],
+    },
   },
 }));
 
